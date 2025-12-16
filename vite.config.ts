4cfa0aa7edbcfe1bf0340578762d794d1b250a66
Madil23 <161153077+Madil23@users.noreply.github.com>
2025-12-16 18:15:46 +0100

    update files

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Cette ligne est cruciale pour que GitHub Pages trouve vos fichiers
-  // Si votre dépôt s'appelle "madilium-site", mettez base: '/madilium-site/'
-  // Sinon, essayez './' pour un chemin relatif simple.
-  base: './', 
+  // On retire la ligne "base" car tu es sur un domaine principal (madilium.com)
 })
